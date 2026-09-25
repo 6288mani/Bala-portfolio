f838c80a2801b4c6ec3628e106c18c59b1ce1c4e
bala <balavenkatamanikumar@gmail.com>
2026-09-25 18:19:48 +0530

    feat: update chatbot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
 import ContactForm from './ContactForm.jsx';
-import FloatingChatWidget from './FloatingChatWidget.jsx';
 
 export default function App() {
   return (
     <div className="relative text-slate-100">
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="lg:col-span-7">
-          <ContactForm />
-        </div>
+      <main className="w-full">
+        <ContactForm />
       </main>
-
-      {/* Floating WhatsApp chat widget */}
-      <FloatingChatWidget />
     </div>
   );
 }
